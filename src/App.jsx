fc6d1d662d8481c1619235bf78426e93d7f2f895
fasvello <fasvello@gmail.com>
2022-07-19 11:43:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Search from './components/search/search'
+
+function App() {
+
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  )
+}
+
+export default App
