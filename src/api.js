b862cbdf8f02af008a9f391a31fd2b2da3b73a4a
fasvello <fasvello@gmail.com>
2022-07-19 12:29:25 -0300

    search city (unfinished) min 19 video

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "88c26933c7msh6db8589b121f07cp105187jsn74a8de1ac4c8",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,3 +1,31 @@
-export default function Search() {
-  return <h1>Hello</h1>;
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+
+export default function Search({ onSearchChange }) {
+  const [search, setSearch] = useState(null);
+
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
+  };
+
+  const handleOnChange = (searchdata) => {
+    setSearch(searchdata);
+    onSearchChange(searchdata);
+  };
+  return (
+    <AsyncPaginate
+      placeholder="Buscar ciudad"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
 }
