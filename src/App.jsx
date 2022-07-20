1858d6d8c6a002b35a12c8c178030d40aa38b128
fasvello <fasvello@gmail.com>
2022-07-20 14:26:06 -0300

    20220720 14:25

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import './App.css'
-import Search from './components/search/search'
+import "./App.css";
+import Search from "./components/search/search";
+import CurrentWeather from "./components/search/current-weather/current-weather";
 
 function App() {
-
-  const handleOnSearchChange= (searchData)=>{
-    console.log(searchData)
-  }
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData);
+  };
 
   return (
     <div className="container">
-      <Search onSearchChange={handleOnSearchChange}/>
+      <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
