a2d9882d5cf7a1517a12702c6127d696b0ee1d24
fasvello <fasvello@gmail.com>
2022-07-21 13:42:36 -0300

    20220721 13:42 51:06 del video

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./App.css";
 import Search from "./components/search/search";
 import CurrentWeather from "./components/search/current-weather/current-weather";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [currentForecast, setForecast] = useState(null);
+
   const handleOnSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}`
+    );
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      const weatherResponde = response[0].json();
+      const forecastResponse = response[1].json();
+    });
   };
 
   return (
--- src/components/search/current-weather/current-weather.js
+++
@@ -1,7 +0,0 @@
-import "./current-weather.css";
-
-const CurrentWeather = () => {
-  return "hellow";
-};
-
-export default CurrentWeather;
